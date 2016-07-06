df762b5127ddc57973742b16e401413e6b53fe54
Vijendra Kulhade <vijendra07kulhade@gmail.com>
2016-07-06 10:20:21 -0700

    elastic search with local node running

--- src/main/java/com/kulhade/controllers/HomeController.java
+++ src/main/java/com/kulhade/controllers/HomeController.java
@@ -1,10 +1,22 @@
 package com.kulhade.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kulhade.entity.User;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by vkulhade on 6/8/2016.
@@ -12,6 +24,19 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
+    @Autowired
+    @Qualifier("esClient")
+    Client client;
+
+    /*@PostConstruct
+    void init(){
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .put("cluster.name", "myCluster")
+                .put("client.transport.sniff", true).build();
+        TransportClient client =    new TransportClient(settings);
+        client.addTransportAddress(new InetSocketTransportAddress("localhost",9300));
+        this.client=client;
+    }*/
     @RequestMapping("/welcome")
     public ModelAndView helloWorld() {
 
@@ -21,7 +46,12 @@ public class HomeController {
     }
 
     @RequestMapping("/user")
-    public @ResponseBody User get(){
-        return new User("Vijendra","Kulhade",100L);
+    public @ResponseBody IndexResponse get() throws JsonProcessingException{
+        User user = new User("Vijendra","Kulhade",100L);
+        IndexRequest indexRequest = new IndexRequest("users","user",user.getFirstName());
+        com.fasterxml.jackson.databind.ObjectMapper objectMapper = new com.fasterxml.jackson.databind.ObjectMapper();
+        indexRequest.source(objectMapper.writeValueAsString(user));
+        IndexResponse response = client.index(indexRequest).actionGet();
+        return response;
     }
 }
